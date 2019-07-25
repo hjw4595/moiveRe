74bcfdfc078fe40ee977939ef00f27f6c42d046d
hjw4595 <hongjung73@gmail.com>
2019-07-25 15:26:47 +0900

    use apollo ->  react + graphql

--- MOVIERE-CLIENT/src/App.js
+++ MOVIERE-CLIENT/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <main>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+            </main>
+        </Router>
+      </ApolloProvider>
+    );
+  }
 }
-
 export default App;
